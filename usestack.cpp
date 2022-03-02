f2624af50208d73ddfecda49937c17c9b6fc34cf
Albert Yu <92063396+albertyu03@users.noreply.github.com>
2022-03-02 22:17:58 +0000

    tested with usestack.cpp

--- usestack.cpp
+++ usestack.cpp
@@ -1,5 +1,5 @@
 // usestack.cpp - for CS 24 lab practice using stacks
-// NAME(S), DATE
+// Albert Yu (3/2/2022)
 
 #include "intstack.h"
 #include <iostream>
@@ -10,7 +10,9 @@ int main() {
 
     s.push(10);
     s.push(20);
-
+    s.push(30);
+    s.push(40);
+    s.pop();
     while (!s.empty()) {
         cout << s.top() << endl;
         s.pop();
